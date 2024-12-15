d354fef69fd9aedc763f31cf78cfdf7e2c2b48a2
ReemaAbuAlRub <reema_maen@hotmail.com>
2024-12-16 01:23:54 +0300

    first

--- main.py
+++ main.py
@@ -8,20 +8,23 @@ def get_setup_input() -> any:
     if number  <=  0:
         raise ValueError("Invalid number of Questions")
 
-    url = input("Insert your LinkedIn Account URL: ")
+    url = input("Insert your LinkedIn Account URL: ").strip()
 
     difficulty = input("Difficulty of the Quiz (easy, medium, hard): ")
 
-    if str.lower(difficulty) not in  ['easy', 'medium', 'hard']:
+    if str.lower(difficulty).strip() not in  ['easy', 'medium', 'hard']:
         raise ValueError("Invalid  difficulty level")
 
     return number , url, difficulty
 
-def check_answer(answer: str,correct_answer: str) -> bool:
-    if answer == correct_answer:
-        return True
+def check_answer(options, answer: int,correct_answer: str) -> bool:
+    if 1 > answer & answer > 4 :
+        raise ValueError("Invalid Answer")
     else:
-        return False
+        if options[answer - 1] == correct_answer:
+            return True
+        else:
+            return False
 
 if __name__ == "__main__":
     try:
@@ -36,15 +39,18 @@ if __name__ == "__main__":
         cleaned_quiz_response = re.sub(r'```[a-zA-Z]*\n|\n```', '', quiz_response["quiz"]).strip()
         quiz = json.loads(cleaned_quiz_response)
 
-        print("\nGenerated Quiz:\n" + "-" * 50)
+        print("\nGenerated Quiz:\n")
         for i, item in enumerate(quiz, 1):
             print(f"Question {i}: {item['question']}")
             print("Options:")
             for j, option in enumerate(item['responses'], 1):
                 print(f"  {j}. {option}")
-            answer = input("Enter your Answer: ")
-
-            status=check_answer(answer,item['correct'])
+            answer = input("Select the Correct Answer (1,2,3,4): ")
+            
+            if type(answer) != "str":
+                raise ValueError("Invalid Answer")
+            
+            status=check_answer(item['responses'],int(answer),item['correct'])
 
             if status:
                 print("Correct!\n" + "-" * 50)
