6e0354502cbc0ec56a175a346df7271acca32b7d
ReemaAbuAlRub <reema_maen@hotmail.com>
2024-12-16 23:51:34 +0300

    commit

--- src/mcqgenerator/utils.py
+++ src/mcqgenerator/utils.py
@@ -6,6 +6,7 @@ import nltk
 import spacy
 from langchain_community.chat_models import openai
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
@@ -20,18 +21,25 @@ env_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "
 load_dotenv(dotenv_path=env_path)
 
 def get_linkein_api():
-    PASSWORD=os.getenv('PASSWORD')
-    EMAIL=os.getenv('EMAIL')
-    linkedin = Linkedin(EMAIL,PASSWORD)
+    password=os.getenv('PASSWORD')
+    email=os.getenv('EMAIL')
+    linkedin = Linkedin(email,password)
     return linkedin
 
-def extract_profile(url):
-    sections=[part for part in url.split('/') if part]
-    return sections[-1]
+def is_url(input_string: str) -> bool :
+    result = urlparse(input_string)
+    return all([result.scheme, result.netloc])
+
+def extract_profile(url: str) -> str:
+    if is_url(url):
+        sections=[part for part in url.split('/') if part]
+        return sections[-1]
+    else:
+        raise ValueError("Invalid URL")
  
 
 def fetch_profile_data(url):
-    linkedin=get_linkein_api()
+    linkedin = get_linkein_api()
     username=extract_profile(url)
     profile = linkedin.get_profile(username)
 
@@ -66,31 +74,3 @@ def fetch_profile_data(url):
     except Exception as e:
         logging.error(e)
 
-def get_quiz(quiz_str):
-    try:
-            quiz_dict=json.loads(quiz_str)
-            quiz_table_data=[]
-            
-            # iterate over the quiz dictionary and extract the required information
-            for key,value in quiz_dict.items():
-                mcq=value["questions"]
-                options=" || ".join(
-                    [
-                        f"{option}-> {option_value}" for option, option_value in value["responses"].items()
-                    
-                    ]
-                )
-                
-                correct=value["correct"]
-                quiz_table_data.append({"MCQ": mcq,"Choices": options, "Correct": correct})
-            
-            return quiz_table_data
-        
-    except Exception as e:
-        traceback.print_exception(type(e), e, e.__traceback__)
-        return False
-    
-def create_mcq_dataframe(questions):
-    # Assuming questions is a list of dictionaries with question details
-    df = pd.DataFrame(questions, columns=['questions', 'responses', 'correct','answers'])
-    return df
